0029e7b5a8383f451c2490f04cf3bcde58d5da01
Ubuntu <douglas125@VMDL.vrzisskhye0ehnnremawv2revb.cx.internal.cloudapp.net>
2018-08-27 01:40:06 +0000

    Fixed epsilon in dice loss

--- ModelUNet.py
+++ ModelUNet.py
@@ -193,7 +193,7 @@ def dice_coef(y_true, y_pred, smooth=1):
          =  2*sum(|A*B|)/(sum(A^2)+sum(B^2))
     ref: https://arxiv.org/pdf/1606.04797v1.pdf
     """
-    smooth = 10.01
+    smooth = 0.81
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
     return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
 
